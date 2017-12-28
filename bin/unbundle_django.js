f1e09dc60cd163cee46c0427837119cf1a993180
Jon McClure <jmcclure@politico.com>
2017-12-28 18:33:36 -0500

    update unbundle script. 0.3.1

--- bin/unbundle_django.js
+++ bin/unbundle_django.js
@@ -4,8 +4,8 @@ const path = require('path');
 // Change working directory to lib
 process.chdir(path.join(__dirname, '/../'));
 
-vfs.src('./dist/js/**/*.js').pipe(vfs.dest('./../../../static/chartwerk/js'));
-vfs.src('./dist/css/**/*.css').pipe(vfs.dest('./../../../static/chartwerk/css'));
+vfs.src('./dist/js/**/*').pipe(vfs.dest('./../../../static/chartwerk/js'));
+vfs.src('./dist/css/**/*').pipe(vfs.dest('./../../../static/chartwerk/css'));
 vfs.src('./dist/img/**/*').pipe(vfs.dest('./../../../static/chartwerk/img'));
 vfs.src('./dist/fonts/**/*').pipe(vfs.dest('./../../../static/chartwerk/fonts'));
 vfs.src(['./src/templates/**/*.html', '!./**/index.html', '!./**/base.html'])
